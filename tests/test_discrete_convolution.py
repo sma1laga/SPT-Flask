029682caaa8688b096c5076555e14fba44d56cb3
LethatKataster <tarik.smailagic@fau.de>
2025-07-16 22:57:27 +0200

    tests updated to adapt to new brackets []

--- tests/test_discrete_convolution.py
+++ tests/test_discrete_convolution.py
@@ -2,7 +2,9 @@ import numpy as np
 from pages.discrete_convolution import compute_discrete_convolution
 
 def test_shifted_rect_is_not_zero():
-    res = compute_discrete_convolution('rect_3(k-29)', 'rect_3(k+1)')
+    # The implementation now expects the discrete rectangle notation using
+    # square brackets, e.g. ``rect_3[k-29]``.  Adapt the test accordingly.
+    res = compute_discrete_convolution('rect_3[k-29]', 'rect_3[k+1]')
     y1 = np.array(res['y1'])
     assert np.any(y1 != 0)
     y_conv = np.array(res['y_conv'])
