25f213a186900c46d78d9e93d0e7d5a6e506e5ae
LethatKataster <tarik.smailagic@fau.de>
2025-12-09 11:17:40 +0100

    IVC demos hotfixes

--- pages/demos/b_prediction.py
+++ pages/demos/b_prediction.py
@@ -1,28 +1,23 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-import base64
-import io
-from flask import Blueprint, current_app, render_template
-from PIL import Image
-from .demo_images import cached_demo_image
+from flask import Blueprint, render_template, url_for
+
 
 @dataclass
 class BPredictionDefaults:
-    image_src: str
-
+    image_prev: str
+    image_curr: str
+    image_next: str
 
 b_prediction_bp = Blueprint("b_prediction", __name__, template_folder="././templates")
 
 
 @b_prediction_bp.route("/", methods=["GET"], endpoint="page")
 def index():
-    _, image_path = cached_demo_image(current_app.static_folder)
-
-    with Image.open(image_path) as img:
-        with io.BytesIO() as buffer:
-            img.convert("RGB").save(buffer, format="PNG")
-            encoded_image = base64.b64encode(buffer.getvalue()).decode()
-
-    defaults = BPredictionDefaults(image_src=f"data:image/png;base64,{encoded_image}")
+    defaults = BPredictionDefaults(
+        image_prev=url_for("static", filename="images/np1.png", _external=False),
+        image_curr=url_for("static", filename="images/np2.png", _external=False),
+        image_next=url_for("static", filename="images/np3.png", _external=False),
+    )
     return render_template("demos/b_prediction.html", defaults=defaults)
