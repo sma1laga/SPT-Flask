68f520ebd5c73e3074b3869aed2bddb0523534d4
LethatKataster <tarik.smailagic@fau.de>
2026-02-11 16:02:20 +0100

    remote code execution vulnerability

--- utils/eval_helpers.py
+++ utils/eval_helpers.py
@@ -1,5 +1,4 @@
-import traceback
-
+import ast
 
 def error_data(prefix: str, exc: Exception) -> dict:
     """Return standardized error info with message and position."""
@@ -14,11 +13,70 @@ def error_data(prefix: str, exc: Exception) -> dict:
     return {"error": msg, "pos": pos}
 
 
-def safe_eval(expression, allowed_names):
-    bytecode = compile(expression, "<string>", "eval")
+class _SafeExpressionValidator(ast.NodeVisitor):
+    """Allow only a small math AST subset for user expressions"""
 
-    # check for not explicitly allowed names
-    for name in bytecode.co_names:
-        if name not in allowed_names:
-            raise NameError(f"Use of {name} not allowed!")
+    _allowed_nodes = (
+        ast.Expression,
+        ast.BinOp,
+        ast.UnaryOp,
+        ast.BoolOp,
+        ast.Compare,
+        ast.IfExp,
+        ast.Call,
+        ast.Name,
+        ast.Load,
+        ast.Constant,
+        ast.Tuple,
+        ast.List,
+        ast.Subscript,
+        ast.Slice,
+        ast.Index,
+        ast.keyword,
+        # operators
+        ast.Add,
+        ast.Sub,
+        ast.Mult,
+        ast.Div,
+        ast.FloorDiv,
+        ast.Mod,
+        ast.Pow,
+        ast.USub,
+        ast.UAdd,
+        ast.And,
+        ast.Or,
+        ast.Not,
+        ast.Eq,
+        ast.NotEq,
+        ast.Lt,
+        ast.LtE,
+        ast.Gt,
+        ast.GtE,
+    )
+
+    def __init__(self, allowed_names):
+        super().__init__()
+        self.allowed_names = set(allowed_names)
+
+    def generic_visit(self, node):
+        if not isinstance(node, self._allowed_nodes):
+            raise ValueError(f"Expression element '{type(node).__name__}' is not allowed.")
+        super().generic_visit(node)
+
+    def visit_Name(self, node):
+        if node.id not in self.allowed_names:
+            raise NameError(f"Use of {node.id} not allowed!")
+
+    def visit_Call(self, node):
+        if not isinstance(node.func, ast.Name):
+            raise ValueError("Only direct function calls are allowed.")
+        if node.func.id not in self.allowed_names:
+            raise NameError(f"Use of {node.func.id} not allowed!")
+        self.generic_visit(node)
+
+
+def safe_eval(expression, allowed_names):
+    parsed = ast.parse(expression, mode="eval")
+    _SafeExpressionValidator(allowed_names).visit(parsed)
+    bytecode = compile(parsed, "<string>", "eval")
     return eval(bytecode, {"__builtins__": {}}, allowed_names)
